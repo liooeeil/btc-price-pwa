b98974165c00bed2e2a32861039d3828059f84b8
liooeeil <lin407610@gmail.com>
2026-09-25 22:15:52 +0800

    Fix PWA cache paths

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'tide-btc-shell-v3';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './alert-rules.js', './alert-runtime.js', './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png'];
+const CACHE_NAME = 'tide-btc-shell-v4';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './alert-rules.js', './alert-runtime.js', './icon-180.png', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
   self.skipWaiting();
