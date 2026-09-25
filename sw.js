932b39acedef62cc7b17866ea10d185b29617e5c
liooeeil <lin407610@gmail.com>
2026-09-25 23:01:33 +0800

    Refresh cached alert schedule

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tide-btc-shell-v4';
+const CACHE_NAME = 'tide-btc-shell-v5';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './alert-rules.js', './alert-runtime.js', './icon-180.png', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
